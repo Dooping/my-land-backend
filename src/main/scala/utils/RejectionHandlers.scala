9f59e2e3221a141cf03fed11002ff6a6bc005f26
David Gago <davidopg@gmail.com>
2022-08-18 15:13:21 +0100

    added template route tests, closes #16

--- src/main/scala/utils/RejectionHandlers.scala
+++ src/main/scala/utils/RejectionHandlers.scala
@@ -3,18 +3,27 @@ package utils
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.AuthenticationFailedRejection.{CredentialsMissing, CredentialsRejected}
 import akka.http.scaladsl.server.Directives.complete
-import akka.http.scaladsl.server.{AuthenticationFailedRejection, AuthorizationFailedRejection, RejectionHandler}
+import akka.http.scaladsl.server.{AuthenticationFailedRejection, AuthorizationFailedRejection, MalformedRequestContentRejection, MethodRejection, MissingQueryParamRejection, Rejection, RejectionHandler}
 
 object RejectionHandlers {
 
   def authorizationFailedHandler: RejectionHandler = RejectionHandler.newBuilder()
     .handle {
-    case AuthenticationFailedRejection(CredentialsRejected, _) =>
-      complete(StatusCodes.Unauthorized, "invalid token")
-    case AuthenticationFailedRejection(CredentialsMissing, _) =>
-      complete(StatusCodes.Unauthorized, "missing token")
-    case AuthorizationFailedRejection =>
-      complete(StatusCodes.Forbidden)
-  }.result()
+      case AuthenticationFailedRejection(CredentialsRejected, _) =>
+        complete(StatusCodes.Unauthorized, "invalid token")
+      case AuthenticationFailedRejection(CredentialsMissing, _) =>
+        complete(StatusCodes.Unauthorized, "missing token")
+      case AuthorizationFailedRejection =>
+        complete(StatusCodes.Forbidden)
+      case MalformedRequestContentRejection(message, _) =>
+        complete(StatusCodes.BadRequest, message)
+      case MissingQueryParamRejection(parameterName) =>
+        complete(StatusCodes.BadRequest, s"missing parameter: $parameterName")
+    }
+//    .handleAll {
+//      case list: Seq[Rejection] if list.contains(AuthorizationFailedRejection) =>
+//        complete(StatusCodes.Forbidden)
+//    }
+    .result()
 
 }
---
+++ src/test/scala/routes/TemplateRouteSpec.scala
@@ -0,0 +1,161 @@
+package routes
+
+import actors.ObjectType.ObjType
+import actors.ObjectTypeSpec.{generateObjectTypeList, generateObjectTypeListEntity}
+import actors.TemplateSpec.{localeGen, nameGen}
+import actors.UserManagement
+import akka.actor.{Actor, ActorRef, Props}
+import akka.http.scaladsl.model.{ContentTypes, StatusCodes}
+import akka.http.scaladsl.server.Directives.handleRejections
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.pattern.StatusReply._
+import akka.testkit.{TestActor, TestKit, TestProbe}
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import protocols.{ObjectTypeJsonProtocol, TemplateJsonProtocol}
+import utils.JwtHelper.Payload
+import utils.RejectionHandlers
+
+import java.util.Date
+import scala.language.postfixOps
+
+class TemplateRouteSpec extends AnyWordSpecLike
+  with Matchers
+  with BeforeAndAfterAll
+  with ScalatestRouteTest
+  with ObjectTypeJsonProtocol
+  with TemplateJsonProtocol {
+
+  import actors.Template
+  import actors.Template._
+  import actors.UserManagement.LandCommand
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  private val testAdminAuthPayload = Payload("david gago", isAdmin = true)
+  private val testAuthPayload = Payload("david")
+  private val testProbe = TestProbe("user-probe")
+
+  "Any user" should {
+    "get all default and user object templates" in {
+      val landTemplateDefault = generateObjectTypeList
+      val landTemplateFromLands = generateObjectTypeList
+      testProbe.setAutoPilot((sender: ActorRef, msg: Any) => msg match {
+        case GetObjectTypeOptions(username, locale) =>
+          assert(username == testAdminAuthPayload.username)
+          assert(locale == "pt")
+          sender ! ObjectTypeOptionsResponse(Map("Agriculture" -> landTemplateDefault), Set(landTemplateFromLands))
+          TestActor.KeepRunning
+      })
+      Get("/template?locale=pt") ~> TemplateRoute.route(testProbe.ref, testAdminAuthPayload) ~> check {
+        status shouldBe StatusCodes.OK
+        val response = responseAs[ObjectTypeOptionsResponse]
+        assert(response.default("Agriculture") == landTemplateDefault)
+        assert(response.fromLands.contains(landTemplateFromLands))
+      }
+    }
+
+    "get results for default locale when no locale provided" in {
+      val landTemplateDefault = generateObjectTypeList
+      val landTemplateFromLands = generateObjectTypeList
+      testProbe.setAutoPilot((sender: ActorRef, msg: Any) => msg match {
+        case GetObjectTypeOptions(username, locale) =>
+          assert(username == testAdminAuthPayload.username)
+          assert(locale == "en")
+          sender ! ObjectTypeOptionsResponse(Map("Agriculture" -> landTemplateDefault), Set(landTemplateFromLands))
+          TestActor.KeepRunning
+      })
+      Get("/template") ~> TemplateRoute.route(testProbe.ref, testAdminAuthPayload) ~> check {
+        status shouldBe StatusCodes.OK
+        val response = responseAs[ObjectTypeOptionsResponse]
+        assert(response.default("Agriculture") == landTemplateDefault)
+        assert(response.fromLands.contains(landTemplateFromLands))
+      }
+    }
+
+    "fail to do admin operation" in {
+      Delete("/template?locale=en&name=Agriculture") ~> handleRejections(RejectionHandlers.authorizationFailedHandler){
+        TemplateRoute.route(testProbe.ref, testAuthPayload)} ~> check {
+          status shouldBe StatusCodes.Forbidden
+        }
+      }
+  }
+
+  "Admin user" should {
+    "register a new default template" in {
+      val defaultTemplate = generateObjectTypeList
+      val templateName = nameGen.sample.get
+      val templateLocale = localeGen.sample.get
+      testProbe.setAutoPilot((sender: ActorRef, msg: Any) => msg match {
+        case RegisterNewLandTemplate(locale, name, objTypes) =>
+          assert(objTypes == defaultTemplate)
+          assert(locale == templateLocale)
+          assert(name == templateName)
+          sender ! Success()
+          TestActor.KeepRunning
+      })
+      Post("/template", RegisterNewLandTemplate(templateLocale, templateName, defaultTemplate)) ~>
+          TemplateRoute.route(testProbe.ref, testAdminAuthPayload) ~> check {
+        status shouldBe StatusCodes.OK
+      }
+    }
+
+    "fail to register a default template if no entity provided" in {
+      Post("/template") ~> handleRejections(RejectionHandlers.authorizationFailedHandler){
+        TemplateRoute.route(testProbe.ref, testAdminAuthPayload)} ~> check {
+        status shouldBe StatusCodes.BadRequest
+      }
+    }
+
+    "change a default template" in {
+      val defaultTemplate = generateObjectTypeList
+      val templateName = nameGen.sample.get
+      val templateLocale = localeGen.sample.get
+      testProbe.setAutoPilot((sender: ActorRef, msg: Any) => msg match {
+        case ChangeLandTemplate(locale, name, objTypes) =>
+          assert(objTypes == defaultTemplate)
+          assert(locale == templateLocale)
+          assert(name == templateName)
+          sender ! Success()
+          TestActor.KeepRunning
+      })
+      Put("/template", ChangeLandTemplate(templateLocale, templateName, defaultTemplate)) ~>
+        TemplateRoute.route(testProbe.ref, testAdminAuthPayload) ~> check {
+        status shouldBe StatusCodes.OK
+      }
+    }
+
+    "fail to change a default template if no entity provided" in {
+      Put("/template") ~> handleRejections(RejectionHandlers.authorizationFailedHandler){
+        TemplateRoute.route(testProbe.ref, testAdminAuthPayload)} ~> check {
+        status shouldBe StatusCodes.BadRequest
+      }
+    }
+
+    "delete a default template" in {
+      val templateName = nameGen.sample.get
+      val templateLocale = localeGen.sample.get
+      testProbe.setAutoPilot((sender: ActorRef, msg: Any) => msg match {
+        case DeleteLandTemplate(locale, name) =>
+          assert(locale == templateLocale)
+          assert(name == templateName)
+          sender ! Success()
+          TestActor.KeepRunning
+      })
+      Delete(s"/template?locale=$templateLocale&name=$templateName") ~> TemplateRoute.route(testProbe.ref, testAdminAuthPayload) ~> check {
+        status shouldBe StatusCodes.OK
+      }
+    }
+
+    "fail to delete a default template if any parameter is missing" in {
+      Delete("/template") ~> handleRejections(RejectionHandlers.authorizationFailedHandler){
+        TemplateRoute.route(testProbe.ref, testAdminAuthPayload)} ~> check {
+        status shouldBe StatusCodes.BadRequest
+      }
+    }
+  }
+
+}
