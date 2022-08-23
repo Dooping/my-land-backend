7bfe8fe9468c3a185022fa2a6b86343e99b017bf
David Gago <davidopg@gmail.com>
2022-08-23 09:34:32 +0100

    changed object manager to send confirmation messages

--- src/main/scala/actors/ObjectManager.scala
+++ src/main/scala/actors/ObjectManager.scala
@@ -1,6 +1,7 @@
 package actors
 
-import akka.actor.{Actor, ActorLogging, Props, ReceiveTimeout}
+import akka.actor.{Actor, ActorLogging, ActorRef, Props, ReceiveTimeout, Stash, Timers}
+import akka.pattern.StatusReply._
 import akka.persistence.{PersistentActor, RecoveryCompleted}
 
 import scala.concurrent.duration._
@@ -8,6 +9,10 @@ import scala.language.postfixOps
 
 object ObjectManager {
   def props(username: String, landId: Int): Props = Props(new ObjectManager(username, landId))
+
+  case object TimerKey
+  case object TimerTimeout
+  case object Persisted
 
   // Commands
   trait Command
@@ -21,7 +26,10 @@ object ObjectManager {
   case class DeletedLandObject(id: Int)
 
 }
-class ObjectManager(username: String, landId: Int, receiveTimeoutDuration: Duration = 1 hour) extends PersistentActor with ActorLogging {
+class ObjectManager(username: String, landId: Int, receiveTimeoutDuration: Duration = 1 hour) extends Timers
+  with PersistentActor
+  with ActorLogging
+  with Stash {
   context.setReceiveTimeout(receiveTimeoutDuration)
 
   import ObjectManager._
@@ -45,30 +53,62 @@ class ObjectManager(username: String, landId: Int, receiveTimeoutDuration: Durat
       persist(LandObject(currentId, lat, lon, status, typeId)) { event =>
         objects += event.id -> event
         currentId += 1
+        sender ! Success(event)
       }
 
     case DeleteLandObject(id) =>
       log.info(s"Deleting object: $id")
       persist(DeletedLandObject(id)) { event =>
         objects -= event.id
+        sender ! Success()
       }
 
     case cmd @ ChangeLandObject(id, lat, lon, status, typeId) =>
-      log.info(s"Changing object $id to $cmd")
-      persist(LandObject(id, lat, lon, status, typeId)) { event =>
-        objects += event.id -> event
+      objects.get(id).foreach { _ =>
+        log.info(s"Changing object $id to $cmd")
+        persist(LandObject(id, lat, lon, status, typeId)) { event =>
+          objects += event.id -> event
+          sender ! Success(event)
+        }
       }
 
     case DeleteByType(typeId) =>
       log.info(s"Deleting all objects of type $typeId")
       val objectsToDelete = objects.values.filterNot(_.typeId == typeId).map(obj => DeletedLandObject(obj.id))
-      persistAll(objectsToDelete.toSeq) { event =>
-        objects -= event.id
+      if (objectsToDelete.nonEmpty) {
+        timers.startSingleTimer(TimerKey, TimerTimeout, 1 second)
+        persistAll(objectsToDelete.toSeq) { event =>
+          objects -= event.id
+          self ! Persisted
+        }
+        unstashAll()
+        context.become(waitingForDeletes(objectsToDelete.size, sender))
       }
 
     case ReceiveTimeout =>
       log.info(s"[$persistenceId] Actor idle, stopping...")
       context.stop(self)
+
+    case _ => stash()
+  }
+
+  def waitingForDeletes(count: Int, original: ActorRef): Receive = {
+    case Persisted =>
+      if (count <= 1) {
+        original ! Success()
+        context.become(receiveCommand)
+        timers.cancel(TimerKey)
+      } else {
+        unstashAll()
+        context.become(waitingForDeletes(count - 1, original))
+      }
+
+    case TimerTimeout =>
+      original ! Error("Some objects failed to be deleted")
+      unstashAll()
+      context.become(receiveCommand)
+
+    case _ => stash()
   }
 
 }
