061f4df7ccbc77c77944d53b18fe72071e9b4ea6
David Gago <davidopg@gmail.com>
2022-09-03 10:24:36 +0100

    added task actor, closes #4

---
+++ src/main/scala/actors/TaskManager.scala
@@ -0,0 +1,193 @@
+package actors
+
+import actors.TaskType.Destroy
+import akka.actor.{ActorLogging, Props, ReceiveTimeout}
+import akka.pattern.StatusReply.{Error, Success}
+import akka.persistence.PersistentActor
+
+import java.util.Date
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+object TaskManager {
+
+  def props(username: String): Props = Props(new TaskManager(username))
+
+  case class TaskModel(objectId: Int, taskTypeId: Int, priority: Int, notes: String)
+
+  trait Command
+  case object GetAllTasks extends Command
+  case class GetLandTasks(landId: Int) extends Command
+  case class GetLandObjectTasks(landId: Int, objectId: Int)
+  case object GetSeasonTasks extends Command
+  case object GetOpenTasks extends Command
+  case class CreateTask(landId: Int, task: TaskModel)
+  case class CreateTasks(landId: Int, tasks: List[TaskModel])
+  case class ModifyTask(id: Int, taskModel: TaskModel)
+  case class ArchiveTask(id: Int)
+  case class CompleteTask(id: Int)
+  case class DeleteTask(id: Int)
+
+  // Events
+  case class TaskEntity(
+    id: Int,
+    landId: Int,
+    objectId: Int,
+    taskTypeId: Int,
+    priority: Int,
+    notes: String,
+    createdAt: Date,
+    modifiedAt: Date,
+    completedAt: Option[Date] = None,
+    archivedAt: Option[Date] = None
+  )
+  case class Archived(id: Int, archivedAt: Date)
+  case class Completed(id: Int, completedAt: Date)
+  case class Deleted(id: Int)
+}
+class TaskManager(username: String, receiveTimeoutDuration: Duration = 1 hour) extends PersistentActor with ActorLogging {
+  context.setReceiveTimeout(receiveTimeoutDuration)
+
+  import TaskManager._
+
+  var tasks: Map[Int, TaskEntity] = Map()
+  var currentId = 1
+
+  override def persistenceId: String = s"task-manager-$username"
+
+  override def receiveRecover: Receive = {
+    case task: TaskEntity =>
+      tasks += task.id -> task
+      currentId += 1
+
+    case Archived(id, archivedAt) if tasks.contains(id) =>
+      val oldTask = tasks(id)
+      tasks += id -> oldTask.copy(archivedAt = Some(archivedAt), modifiedAt = List(oldTask.modifiedAt, archivedAt).max)
+
+    case Completed(id, completedAt) if tasks.contains(id) =>
+      val oldTask = tasks(id)
+      tasks += id -> tasks(id).copy(completedAt = Some(completedAt), modifiedAt = List(oldTask.modifiedAt, completedAt).max)
+
+    case Deleted(id) =>
+      tasks -= id
+
+    case msg => log.warning(s"Couldn't recover message: $msg")
+
+
+  }
+
+  override def receiveCommand: Receive = {
+    case GetAllTasks =>
+      sender ! tasks.values.toList
+    case GetSeasonTasks =>
+      sender ! tasks.values.toList.filterNot(_.archivedAt.isDefined)
+    case GetOpenTasks =>
+      sender ! tasks.values.toList.filterNot(t => t.completedAt.isDefined || t.archivedAt.isDefined)
+    case GetLandTasks(landId) =>
+      sender ! tasks.values.toList
+        .filter(_.landId == landId)
+        .filterNot(_.archivedAt.isDefined)
+    case GetLandObjectTasks(landId, objectId) =>
+      sender ! tasks.values.toList
+        .filter(_.landId == landId)
+        .filter(_.objectId == objectId)
+        .filterNot(_.archivedAt.isDefined)
+
+
+    case CreateTask(landId, task) =>
+      log.info(s"[$persistenceId] Creating a new task $task")
+      persist(TaskEntity(currentId, landId, task.objectId, task.taskTypeId, task.priority, task.notes, new Date, new Date)) { event =>
+        tasks += event.id -> event
+        currentId += 1
+        sender ! Success(event)
+      }
+
+    case CreateTasks(landId, tasks) =>
+      log.info(s"[$persistenceId] Creating tasks in batch $tasks")
+      val now = new Date
+      val zippedTasks = tasks.zip(LazyList.from(currentId))
+      val entityList = zippedTasks.map { case (t, id) => TaskEntity(id, landId, t.objectId, t.taskTypeId, t.priority, t.notes, now, now) }
+      var resultCounter = 0
+      persistAll(entityList) { event =>
+        resultCounter += 1
+        currentId += 1
+        this.tasks += event.id -> event
+        if (resultCounter == tasks.length) {
+          sender ! Success(entityList)
+        }
+      }
+
+    case ModifyTask(id, task) =>
+      tasks.get(id) match {
+        case Some(oldEntity) =>
+          log.info(s"[$persistenceId] Changing task $id")
+          persist(
+            oldEntity.copy(
+              objectId = task.objectId,
+              taskTypeId = task.taskTypeId,
+              priority = task.priority,
+              notes = task.notes,
+              modifiedAt = new Date
+            )
+          ) { event =>
+            tasks += event.id -> event
+            sender ! Success(event)
+          }
+
+        case None =>
+          log.info(s"[$persistenceId] task $id not found")
+          sender ! Error(s"No task found with id $id")
+      }
+
+    case ArchiveTask(id) =>
+      tasks.get(id) match {
+        case Some(oldTask) =>
+          log.info(s"[$persistenceId] Archiving task $id")
+          persist(Archived(id, new Date)) { event =>
+            val newTask = oldTask.copy(archivedAt = Some(event.archivedAt))
+            tasks += id -> newTask
+            sender ! Success(newTask)
+          }
+        case None =>
+          log.info(s"[$persistenceId] task $id not found")
+          sender ! Error(s"No task found with id $id")
+      }
+
+    case CompleteTask(id) =>
+      tasks.get(id) match {
+        case Some(oldTask) =>
+          log.info(s"[$persistenceId] Completing task $id")
+          persist(Completed(id, new Date)) { event =>
+            val newTask = oldTask.copy(completedAt = Some(event.completedAt))
+            tasks += id -> newTask
+            sender ! Success(newTask)
+          }
+        case None =>
+          log.info(s"[$persistenceId] task $id not found")
+          sender ! Error(s"No task found with id $id")
+      }
+
+    case DeleteTask(id) =>
+      tasks.get(id) match {
+        case Some(_) =>
+          log.info(s"[$persistenceId] Deleting task $id")
+          persist(Deleted(id)) { _ =>
+            sender ! Success()
+          }
+
+        case None =>
+          log.info(s"[$persistenceId] task $id not found")
+          sender ! Error(s"No task found with id $id")
+      }
+
+    case Destroy =>
+      log.info(s"[$persistenceId] Destroying actor and all data")
+      deleteMessages(lastSequenceNr)
+      context.stop(self)
+
+    case ReceiveTimeout =>
+      log.info(s"[$persistenceId] Actor idle, stopping...")
+      context.stop(self)
+
+  }
+}
---
+++ src/test/scala/actors/TaskManagerSpec.scala
@@ -0,0 +1,248 @@
+package actors
+
+import actors.TaskManagerSpec.{genCreateTask, genModifyTask}
+import akka.actor.{Actor, ActorSystem, PoisonPill}
+import akka.pattern.StatusReply._
+import akka.persistence.testkit.PersistenceTestKitPlugin
+import akka.persistence.testkit.scaladsl.PersistenceTestKit
+import akka.testkit.{EventFilter, ImplicitSender, TestKit}
+import com.typesafe.config.ConfigFactory
+import org.scalacheck.Gen
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpecLike
+import org.scalatest.{BeforeAndAfterAll, BeforeAndAfterEach}
+
+import scala.concurrent.duration._
+import scala.language.postfixOps
+
+object TaskManagerSpec {
+  import TaskManager._
+
+  def genTaskModel(objectId: Option[Int] = None): Gen[TaskModel] = for {
+    objId <- objectId.map(Gen.const).getOrElse(Gen.choose(1,5))
+    taskType <- Gen.choose(1, 5)
+    priority <- Gen.choose(1, 5)
+    notes <- Gen.alphaStr
+  } yield TaskModel(objId, taskType, priority, notes)
+  def genCreateTask(landId: Int, objectId: Option[Int] = None): CreateTask = CreateTask(landId, genTaskModel(objectId).sample.get)
+  def genModifyTask(id: Int): ModifyTask = ModifyTask(id, genTaskModel().sample.get)
+  def genCreateTasks(landId: Int): CreateTasks = CreateTasks(
+    landId,
+    Gen.containerOfN[List, TaskModel](Gen.choose(1, 5).sample.get, genTaskModel()).sample.get
+  )
+}
+class TaskManagerSpec
+  extends TestKit(ActorSystem("TaskManagerPackage", PersistenceTestKitPlugin.config.withFallback(ConfigFactory.load().getConfig("interceptingLogMessages"))))
+    with AnyWordSpecLike
+    with BeforeAndAfterAll
+    with BeforeAndAfterEach
+    with ImplicitSender
+    with Matchers {
+
+  override def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+    persistenceTestKit.clearAll()
+  }
+
+  override def afterEach(): Unit = {
+    taskManagerActor ! PoisonPill
+    super.afterEach()
+  }
+
+  import TaskManager._
+
+  private val username = "test"
+  private var taskManagerActor = Actor.noSender
+
+  private val persistenceTestKit: PersistenceTestKit = PersistenceTestKit(system)
+
+  "A CreateTask" should {
+    "create a new task" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      val createTask = genCreateTask(1)
+      taskManagerActor ! createTask
+      expectMsgPF() {
+        case Success(TaskEntity(1, 1, createTask.task.objectId, createTask.task.taskTypeId, createTask.task.priority, createTask.task.notes, _, _, None, None)) =>
+      }
+    }
+
+    "not create an object with the same id as a previous one" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      val createTask = genCreateTask(1)
+      taskManagerActor ! createTask
+      receiveOne(1 second)
+
+      val ct2 = genCreateTask(1)
+      taskManagerActor ! ct2
+      expectMsgPF() {
+        case Success(TaskEntity(2, 1, ct2.task.objectId, ct2.task.taskTypeId, ct2.task.priority, ct2.task.notes, _, _, None, None)) =>
+      }
+    }
+  }
+
+  "A DeleteTask" should {
+    "delete an existing object" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      taskManagerActor ! genCreateTask(2)
+      receiveOne(1 second)
+
+      taskManagerActor ! DeleteTask(1)
+      expectMsg(Success())
+    }
+
+    "fail when the object does not exist" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      EventFilter.info(s"[task-manager-$username] task 1 not found", occurrences = 1) intercept {
+        taskManagerActor ! DeleteTask(1)
+        expectMsg(Error("No task found with id 1"))
+      }
+    }
+  }
+
+  "A ModifyTask" should {
+    "modify an existing object" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      taskManagerActor ! genCreateTask(2)
+      receiveOne(1 second)
+
+      val ct = genModifyTask(1)
+      taskManagerActor ! ct
+      expectMsgPF() {
+        case Success(TaskEntity(ct.id, _, ct.taskModel.objectId, ct.taskModel.taskTypeId, ct.taskModel.priority, ct.taskModel.notes, createdAt, modifiedAt, _, _)) =>
+          assert(modifiedAt.after(createdAt))
+      }
+    }
+
+    "fail when the task does not exist" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      EventFilter.info(s"[task-manager-$username] task 1 not found", occurrences = 1) intercept {
+        taskManagerActor ! genModifyTask(1)
+        expectMsg(Error("No task found with id 1"))
+      }
+    }
+  }
+
+  "An ArchiveTask" should {
+    "archive an existing task" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      taskManagerActor ! genCreateTask(2)
+      receiveOne(1 second)
+
+      taskManagerActor ! ArchiveTask(1)
+      expectMsgPF() {
+        case Success(TaskEntity(_, _, _, _, _, _, _, _, _, Some(_))) =>
+      }
+    }
+
+    "fail when the task does not exist" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      EventFilter.info(s"[task-manager-$username] task 1 not found", occurrences = 1) intercept {
+        taskManagerActor ! ArchiveTask(1)
+        expectMsg(Error("No task found with id 1"))
+      }
+    }
+  }
+
+  "A CompleteTask" should {
+    "set a task as done" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      taskManagerActor ! genCreateTask(2)
+      receiveOne(1 second)
+
+      taskManagerActor ! CompleteTask(1)
+      expectMsgPF() {
+        case Success(TaskEntity(_, _, _, _, _, _, _, _, Some(_), _)) =>
+      }
+    }
+
+    "fail when the task does not exist" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      EventFilter.info(s"[task-manager-$username] task 1 not found", occurrences = 1) intercept {
+        taskManagerActor ! CompleteTask(1)
+        expectMsg(Error("No task found with id 1"))
+      }
+    }
+  }
+
+  "A GetAllTasks" should {
+    "get an empty array when no tasks exist" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      taskManagerActor ! GetAllTasks
+      val tasks = expectMsgType[List[TaskEntity]]
+      assert(tasks.isEmpty)
+    }
+
+    "get all tasks from the user" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      taskManagerActor ! genCreateTask(2)
+      taskManagerActor ! genCreateTask(2)
+      taskManagerActor ! genCreateTask(2)
+      receiveN(3)
+
+      taskManagerActor ! GetAllTasks
+      val tasks = expectMsgType[List[TaskEntity]]
+      assert(tasks.length == 3)
+    }
+  }
+
+  "A GetSeasonTasks" should {
+    "get only non archived tasks" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      taskManagerActor ! genCreateTask(2)
+      taskManagerActor ! ArchiveTask(1)
+      taskManagerActor ! genCreateTask(2)
+      taskManagerActor ! CompleteTask(2)
+      taskManagerActor ! ArchiveTask(2)
+      taskManagerActor ! genCreateTask(2)
+      receiveN(6)
+
+      taskManagerActor ! GetSeasonTasks
+      val tasks = expectMsgType[List[TaskEntity]]
+      assert(tasks.length == 1)
+
+      assert(tasks.head.id == 3)
+    }
+  }
+
+  "A GetOpenTasks" should {
+    "get only non archived and non completed tasks" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      taskManagerActor ! genCreateTask(2)
+      taskManagerActor ! ArchiveTask(1)
+      taskManagerActor ! genCreateTask(2)
+      taskManagerActor ! genCreateTask(2)
+      taskManagerActor ! CompleteTask(3)
+      receiveN(5)
+
+      taskManagerActor ! GetOpenTasks
+      val tasks = expectMsgType[List[TaskEntity]]
+      assert(tasks.length == 1)
+
+      assert(tasks.head.id == 2)
+    }
+  }
+
+  "A GetLandTasks" should {
+    "get only non archived tasks for the selected land" in {
+      taskManagerActor = system.actorOf(TaskManager.props(username))
+      taskManagerActor ! genCreateTask(2)
+      taskManagerActor ! ArchiveTask(1)
+      taskManagerActor ! genCreateTask(1)
+      taskManagerActor ! genCreateTask(2)
+      taskManagerActor ! CompleteTask(3)
+      taskManagerActor ! genCreateTask(2)
+      receiveN(6)
+
+      taskManagerActor ! GetLandTasks(2)
+      val tasks = expectMsgType[List[TaskEntity]]
+
+      tasks should have length 2
+      tasks.map(_.id) should contain only (3, 4)
+    }
+  }
+
+}
