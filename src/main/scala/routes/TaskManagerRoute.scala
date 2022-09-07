180c1a1cf1be4b97bfe6e87e5a3ce04b17bac409
David Gago <davidopg@gmail.com>
2022-09-07 10:15:41 +0100

    fixed task routes, closes #28

--- src/main/scala/routes/TaskManagerRoute.scala
+++ src/main/scala/routes/TaskManagerRoute.scala
@@ -1,13 +1,14 @@
 package routes
 
 import akka.actor.ActorRef
-import akka.http.scaladsl.model.{HttpEntity, HttpResponse, StatusCodes}
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpResponse, StatusCodes}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.pattern.StatusReply._
-import akka.pattern.ask
+import akka.pattern.{StatusReply, ask}
 import akka.util.Timeout
 import protocols.TaskJsonProtocol
+import spray.json._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -31,33 +32,36 @@ object TaskManagerRoute extends TaskJsonProtocol {
           val getTaskFuture = (authenticator ? TaskCommand(username, command))
             .mapTo[List[TaskEntity]]
           complete(getTaskFuture)
-        } ~
-        path(IntNumber) { id =>
-          put {
-            (entity(as[TaskModel]) & pathEndOrSingleSlash) { model =>
-              val changeTaskFuture = (authenticator ? TaskCommand(username, ModifyTask(id, model)))
-                .mapTo[List[TaskEntity]]
-              complete(changeTaskFuture)
-            } ~
-            path("complete") {
-              val changeTaskFuture = (authenticator ? TaskCommand(username, CompleteTask(id)))
-                .mapTo[List[TaskEntity]]
-              complete(changeTaskFuture)
-            } ~
-            path("archive") {
-              val changeTaskFuture = (authenticator ? TaskCommand(username, ArchiveTask(id)))
-                .mapTo[List[TaskEntity]]
-              complete(changeTaskFuture)
-            }
+        }
+      } ~
+      pathPrefix(IntNumber) { id =>
+        put {
+          (entity(as[TaskModel]) & pathEndOrSingleSlash) { model =>
+            val changeTaskFuture = (authenticator ? TaskCommand(username, ModifyTask(id, model)))
+              .mapTo[StatusReply[TaskEntity]]
+              .map(mapToEntity)
+            complete(changeTaskFuture)
           } ~
-          (delete & pathEndOrSingleSlash) {
-            val deleteTaskFuture = (authenticator ? TaskCommand(username, DeleteTask(id)))
-              .map {
-                case Success(_) => HttpResponse(StatusCodes.OK)
-                case Error(reason) => HttpResponse(StatusCodes.BadRequest, entity = HttpEntity(reason.getMessage))
-              }
-            complete(deleteTaskFuture)
+          path("complete") {
+            val changeTaskFuture = (authenticator ? TaskCommand(username, CompleteTask(id)))
+              .mapTo[StatusReply[TaskEntity]]
+              .map(mapToEntity)
+            complete(changeTaskFuture)
+          } ~
+          path("archive") {
+            val changeTaskFuture = (authenticator ? TaskCommand(username, ArchiveTask(id)))
+              .mapTo[StatusReply[TaskEntity]]
+              .map(mapToEntity)
+            complete(changeTaskFuture)
           }
+        } ~
+        (delete & pathEndOrSingleSlash) {
+          val deleteTaskFuture = (authenticator ? TaskCommand(username, DeleteTask(id)))
+            .map {
+              case Success(_) => HttpResponse(StatusCodes.OK)
+              case Error(reason) => HttpResponse(StatusCodes.BadRequest, entity = HttpEntity(reason.getMessage))
+            }
+          complete(deleteTaskFuture)
         }
       }
     }
@@ -74,7 +78,34 @@ object TaskManagerRoute extends TaskJsonProtocol {
         val getTaskFuture = (authenticator ? TaskCommand(username, GetLandTasks(landId)))
           .mapTo[List[TaskEntity]]
         complete(getTaskFuture)
+      } ~
+      post {
+        entity(as[TaskModel]) { task =>
+          val createTaskFuture = (authenticator ? TaskCommand(username, CreateTask(landId, task)))
+            .mapTo[StatusReply[TaskEntity]]
+            .map {
+              case Success(entity: TaskEntity) =>
+                HttpResponse(StatusCodes.Created, entity = HttpEntity(ContentTypes.`application/json`, entity.toJson.prettyPrint))
+              case Error(reason) => HttpResponse(StatusCodes.BadRequest, entity = HttpEntity(reason.getMessage))
+            }
+          complete(createTaskFuture)
+        } ~
+        entity(as[List[TaskModel]]) { tasks =>
+          val createTasksFuture = (authenticator ? TaskCommand(username, CreateTasks(landId, tasks)))
+            .mapTo[StatusReply[List[TaskEntity]]]
+            .map {
+              case Success(entities: List[TaskEntity]) =>
+                HttpResponse(StatusCodes.Created, entity = HttpEntity(ContentTypes.`application/json`, entities.toJson.prettyPrint))
+              case Error(reason) => HttpResponse(StatusCodes.BadRequest, entity = HttpEntity(reason.getMessage))
+            }
+          complete(createTasksFuture)
+        }
       }
     }
+  }
+
+  def mapToEntity(response: StatusReply[TaskEntity]): HttpResponse = response match {
+    case Success(entity: TaskEntity) => HttpResponse(StatusCodes.OK, entity = HttpEntity(ContentTypes.`application/json`, entity.toJson.prettyPrint))
+    case Error(reason) => HttpResponse(StatusCodes.BadRequest, entity = HttpEntity(reason.getMessage))
   }
 }
